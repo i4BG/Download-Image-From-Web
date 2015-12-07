736c70792c49aeee94fd50ba33c476f6c87a4e41
Ivaylo Todorov <ivaylo.k.todorov@me.com>
2015-12-07 22:00:48 +0200

    DownloadImageFromWeb-iOS
    how to download images from web on iOS

--- Download Image From Web/ViewController.swift
+++ Download Image From Web/ViewController.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var image: UIImageView!
+ 
+        override func viewDidLoad() {
+            
+            super.viewDidLoad()
+            
+            // download image from web
+            let url = NSURL(string: "https://developer.apple.com/assets/elements/icons/128x128/swift.png")
+            
+            let urlRequest = NSURLRequest(URL: url!)
+            
+            NSURLConnection.sendAsynchronousRequest(urlRequest, queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
+                
+                if error != nil {
+                    
+                    print(error)
+                    
+                } else {
+                
+             let test = UIImage(data: data!)
+                    
+                    self.image.image = test
+                    
+                    // save image in the App
+                    var documentsDirectory:String?
+                    
+                    var paths:[AnyObject] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+                    
+                    if paths.count > 0 {
+                        
+                        documentsDirectory = paths[0] as? String
+                        
+                        var savePath = documentsDirectory! + "/swift.png"
+                        
+                        NSFileManager.defaultManager().createFileAtPath(savePath, contents: data, attributes: nil)
+                        
+                        //use allready saved image
+                        self.image.image = UIImage(named: savePath)
+
+                    }
+            
+            }
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
